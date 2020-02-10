2b7174be8d50081ca98919b44c7008b6b6878fbb
Robin Lillqvist <robin.lillqvist@gmail.com>
2020-02-10 15:20:58 +0100

    finished the demo functionality with mock data and functions to fetch title and content from instance variables

--- spec/features/user_can_see_a_of_articles_spec.rb
+++ spec/features/user_can_see_a_of_articles_spec.rb
@@ -7,7 +7,7 @@ feature 'List articles on index page' do
     before do
 
       create(:article, title: 'A breaking news item')
-      create(:article, title: 'Some breaking action')  
+      create(:article, title: 'Some really breaking action')  
     
       visit root_path
     end
