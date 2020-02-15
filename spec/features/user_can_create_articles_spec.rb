3617fb957b691e465bf598446ac730d05830c6e0
Janko <mailodjanka@gmail.com>
2020-02-15 16:26:22 +0100

    Added a new link 'Create Article' that submits the article

--- spec/features/user_can_create_articles_spec.rb
+++ spec/features/user_can_create_articles_spec.rb
@@ -10,13 +10,13 @@ feature 'User can create articles' do
 context "Successfully create an article [Happy Path]" do
     before do
         fill_in "Title", with: "Happy holidays"
-        fill_in "Content", with: "Buy your gifts now!"
+        fill_in "Text", with: "Buy your gifts now!"
         click_on "Create Article"
     end
 
     it 'User should be on article show page' do
         article = Article.find_by(title: 'Happy holidays')
-        expect(current_path).to eq article_path(article)
+        expect(current_path).to eq articles_path
     end
 end
 end
