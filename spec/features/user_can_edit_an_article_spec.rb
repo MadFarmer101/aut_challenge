ea85d58f2829cd0cd56f48aeef19acd92940b655
Janko <mailodjanka@gmail.com>
2020-02-16 12:32:23 +0100

    Refactored Edit Article tests. Tested that a user can edit title and/or content

--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -2,19 +2,32 @@ require 'rails_helper'
 require 'factory_bot'
 
 feature 'Edit an article' do
-    context 'Successfully edit an article [Happy Path' do
+    context 'Successfully edit an article [Happy path]' do
       before do
   
         create(:article, title: 'Unbelievable story', content: 'You will not believe your ears')
+
         visit articles_path
-        click_on 'Edit Article'
+        click_on "Edit"
         fill_in "Content", with: "You will not believe your ears. Edit! You will pretty much beleive your ears once you read this"
-        click_on "Create Article"
+        click_on "Edit Article"
       end
 
-      it 'User should see article title' do
+      it 'Content' do
         expect(page).to have_content 'Edit! You will pretty much beleive your ears once you read this'
-     end
+      end
+
+      before do
+
+        visit articles_path
+        click_on "Edit"
+        fill_in "Title", with: "Plain story"
+        click_on "Edit Article"
+      end
+
+      it 'Title' do
+        expect(page).to have_content 'Plain story'
+      end
 
     end
-  end
+end
