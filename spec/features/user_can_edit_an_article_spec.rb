296a81011dbf26ac117dd87903165f5d2400cfdc
Janko <mailodjanka@gmail.com>
2020-02-16 12:04:02 +0100

    Passed the test that article can be edited once created

--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -8,15 +8,13 @@ feature 'Edit an article' do
         create(:article, title: 'Unbelievable story', content: 'You will not believe your ears')
         visit articles_path
         click_on 'Edit Article'
+        fill_in "Content", with: "You will not believe your ears. Edit! You will pretty much beleive your ears once you read this"
+        click_on "Create Article"
       end
 
-      it 'User should be on edit article form' do
-        article = Article.find_by(title: 'Unbelievable story')
-        expect(current_path).to eq edit_article_path(article)
-    end
-  
       it 'User should see article title' do
-    expect(page).to have_content 'You will'
+        expect(page).to have_content 'Edit! You will pretty much beleive your ears once you read this'
      end
+
     end
   end
