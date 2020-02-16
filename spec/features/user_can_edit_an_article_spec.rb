a35b2b0dc55d1cd5fb62941aaeb05d53bf912526
Janko <mailodjanka@gmail.com>
2020-02-16 12:53:32 +0100

    Tested sad paths in editing articles: title and/or content empty

--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -3,31 +3,56 @@ require 'factory_bot'
 
 feature 'Edit an article' do
     context 'Successfully edit an article [Happy path]' do
-      before do
-  
+        before do
         create(:article, title: 'Unbelievable story', content: 'You will not believe your ears')
 
         visit articles_path
         click_on "Edit"
         fill_in "Content", with: "You will not believe your ears. Edit! You will pretty much beleive your ears once you read this"
         click_on "Edit Article"
-      end
+        end
 
-      it 'Content' do
+        it 'Content' do
         expect(page).to have_content 'Edit! You will pretty much beleive your ears once you read this'
-      end
+        end
 
-      before do
+        before do
 
         visit articles_path
         click_on "Edit"
         fill_in "Title", with: "Plain story"
         click_on "Edit Article"
-      end
+        end
 
-      it 'Title' do
+        it 'Title' do
         expect(page).to have_content 'Plain story'
-      end
+        end
+    end
 
+    context "User doesn't enter a title or content when editing an article [Sad Path]" do
+        before do
+        create(:article, title: 'Unbelievable story', content: 'You will not believe your ears')
+
+        visit articles_path
+        click_on "Edit"
+        fill_in "Title", with: ""
+        fill_in "Content", with: "Now that I think about it..."
+        click_on "Edit Article"
+        end
+    
+        it 'User should see error message for missing title' do
+        expect(page).to have_content "error prohibited this article from being saved"
+        end
+    
+    
+        before do
+        fill_in "Title", with: "Plain story"
+        fill_in "Content", with: ""
+        click_on "Edit Article"
+        end
+    
+        it 'User should see error message for missing content' do
+        expect(page).to have_content "Content can't be blank"
+        end
     end
 end
