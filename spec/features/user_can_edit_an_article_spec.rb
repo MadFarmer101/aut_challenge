4918dc25afa658bc3cf85d7e9159fcd3be39a463
Janko <mailodjanka@gmail.com>
2020-02-16 11:41:04 +0100

    Added an Update method to the article controller

--- spec/features/user_can_edit_an_article_spec.rb
+++ spec/features/user_can_edit_an_article_spec.rb
@@ -7,11 +7,17 @@ feature 'Edit an article' do
   
         create(:article, title: 'Unbelievable story', content: 'You will not believe your ears')
         visit articles_path
-        click_on 'Edit'
+        click_on 'Unbelievable story'
+        click_on 'Edit Article'
       end
+
+      it 'User should be on article show page' do
+        article = Article.find_by(title: 'Unbelievable story')
+        expect(current_path).to eq edit_article_path
+    end
   
       it 'User should see article title' do
-        expect(page).to have_content 'story'
-    end
+       expect(page).to have_content 'story'
+     end
     end
   end
